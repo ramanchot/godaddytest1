6497c5ce5c516fb75d2a1a33033fe7b4ba73da40
ramanchot <raman94@live.com>
2025-11-21 15:25:58 +0530

    Update getTenant.js

--- api/getTenant.js
+++ api/getTenant.js
@@ -3,10 +3,17 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const clientPromise = client.connect();
 
 export default async function handler(req, res) {
-  const propertyId = req.query.propertyId;
+  try {
+    const propertyId = req.query.propertyId;
 
-  const db = (await clientPromise).db("RamanDB");
-  const list = await db.collection("tenants").find({ propertyId }).toArray();
-  res.json(list);
+    const db = (await clientPromise).db("RamanDB");
+    const list = await db.collection("tenants")
+      .find({ propertyId })
+      .toArray();
+
+    res.json(list);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
-
