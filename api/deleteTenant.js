7362eb653d796cbec61b1dcda57c492cc861eea4
ramanchot <raman94@live.com>
2025-11-21 15:26:18 +0530

    Update deleteTenant.js

--- api/deleteTenant.js
+++ api/deleteTenant.js
@@ -3,10 +3,15 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const clientPromise = client.connect();
 
 export default async function handler(req, res) {
-  const id = req.query.id;
+  try {
+    const id = req.query.id;
 
-  const db = (await clientPromise).db("RamanDB");
-  await db.collection("tenants").deleteOne({ _id: new ObjectId(id) });
+    const db = (await clientPromise).db("RamanDB");
+    await db.collection("tenants").deleteOne({ _id: new ObjectId(id) });
 
-  res.json({ success: true });
+    res.json({ success: true });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
