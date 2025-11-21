b8357c3cd1c794d09dd6f63eff72d7f2d128b14d
ramanchot <raman94@live.com>
2025-11-21 15:06:38 +0530

    Add API endpoint to insert property into MongoDB

---
+++ api/addProperty.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+const client = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = client.connect();
+
+export default async function handler(req, res) {
+  let body = "";
+  for await (const c of req) body += c;
+  const data = JSON.parse(body);
+
+  const db = (await clientPromise).db("RamanDB");
+
+  const result = await db.collection("properties").insertOne({
+    name: data.name
+  });
+
+  res.json({ success: true, id: result.insertedId });
+}
