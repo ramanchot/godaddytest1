3d3701fcb78c7377971f39d2746e5b5c5188c0cf
ramanchot <raman94@live.com>
2025-12-03 13:17:50 +0530

    test commit for load property

--- custom-property-manager.js
+++ custom-property-manager.js
@@ -12,3 +12,29 @@ async function addProperty() {
   alert("Property added");
   document.getElementById("propertyName").value = "";
 }
+/* Load Properties into dropdown */
+async function loadProperties() {
+  const res = await fetch("/api/getProperties");
+  const props = await res.json();
+
+  //let html = `<option value=''>Select</option>`;
+  props.forEach(
+    //(p) => (html += `<input type='text' value='${p._id}'>${p.name}</input>`)
+    alert(`ID: ${p._id} Name: ${p.name}`)
+  );
+
+ // document.getElementById("propertyDropdown").innerHTML = html;
+}
+
+/* Load Properties into dropdown */
+async function loadProperties1() {
+  const res = await fetch("/api/getProperties");
+  const props = await res.json();
+
+  let html = `<option value=''>Select</option>`;
+  props.forEach(
+    (p) => (html += `<option value='${p._id}'>${p.name}</option>`)
+  );
+
+  document.getElementById("propertyDropdown").innerHTML = html;
+}
