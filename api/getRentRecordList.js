ed2e22876fc9c867dcc570d3f7c5a0fc6561500c
ramanchot <raman94@live.com>
2025-12-24 14:06:27 +0530

    made dynamic

--- api/getRentRecordList.js
+++ api/getRentRecordList.js
@@ -4,7 +4,14 @@ const clientPromise = client.connect();
 
 export default async function handler(req, res) {
   try {
-    const query={month:2, year:2025};
+    
+    
+    const { month, year } = req.query; 
+    /*const query={month:2, year:2025};*/
+    const query = {
+      month: Number(month),
+      year: Number(year)
+    };
     const db = (await clientPromise).db("RamanDB");
     const list = await db.collection("rentRecords").find(query).toArray();
     res.json(list);
