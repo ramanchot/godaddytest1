c9f0842659c17d4e52a4880ce3903e0268e145b1
ramanchot <raman94@live.com>
2026-02-26 20:35:55 +0530

    common file for functions

---
+++ api/main.js
@@ -0,0 +1,46 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = client.connect();
+
+export default async function handler(req, res) {
+  try {
+    if (req.method !== "POST") {
+      return res.status(405).json({ error: "Only POST allowed" });
+    }
+
+    const db = (await clientPromise).db("RamanDB");
+    const { action, data } = req.body;
+
+    switch (action) {
+
+      case "ADD_PROPERTY":
+        const propertyResult = await db.collection("properties").insertOne({
+          name: data.name
+        });
+
+        return res.json({
+          success: true,
+          id: propertyResult.insertedId
+        });
+
+      case "ADD_TENANT":
+        const tenantResult = await db.collection("tenants").insertOne({
+          propertyId: data.propertyId,
+          name: data.name
+        });
+
+        return res.json({
+          success: true,
+          id: tenantResult.insertedId
+        });
+
+      default:
+        return res.status(400).json({ error: "Invalid action" });
+    }
+
+  } catch (err) {
+    console.error("Mongo Error:", err);
+    res.status(500).json({ error: err.message });
+  }
+}
