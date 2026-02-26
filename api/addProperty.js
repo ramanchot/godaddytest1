aea05d867e84c1fe2061b103b8e3843c90eaefff
ramanchot <raman94@live.com>
2026-02-26 16:08:50 +0530

    change to addproperty.js to use req.body directly instead of reading the stream, and add console logs for debugging. Also added MongoDB URI to .env.local.

--- api/addProperty.js
+++ api/addProperty.js
@@ -1,13 +1,16 @@
 import { MongoClient } from "mongodb";
 const client = new MongoClient(process.env.MONGODB_URI);
 const clientPromise = client.connect();
+console.log("Mongo URI:", process.env.MONGODB_URI);
 
 export default async function handler(req, res) {
   try {
-    let body = "";
-    for await (const c of req) body += c;
-    const data = JSON.parse(body);
-
+    console.log("METHOD:", req.method);
+    console.log("BODY:", req.body);
+    //let body = "";
+    //for await (const c of req) body += c;
+    //const data = JSON.parse(body);
+    const data = req.body;
     const db = (await clientPromise).db("RamanDB");
 
     const result = await db.collection("properties").insertOne({
@@ -17,6 +20,7 @@ export default async function handler(req, res) {
     res.json({ success: true, id: result.insertedId });
 
   } catch (err) {
+    console.error("Mongo Insert Error:", err);
     res.status(500).json({ error: err.message });
   }
 }
